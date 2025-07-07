8c9e5fee49f3763be6089a37d6dbcf8c0349d6c4
Nadayhia <Nadayhia@outlook.sa>
2025-07-07 15:54:23 +0300

    testcontroller

---
+++ PetHospitalApi/Areas/Admin/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PetHospitalApi.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("api/[Area]/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "This is a test response from the Admin area." });
+        }
+    }
+}
